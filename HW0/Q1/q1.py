d2b47b932a99ea029f2e54472e79b47cdc4aa033
Howard WU <Wchoward@users.noreply.github.com>
2019-09-27 09:30:16 +0800

    google tensorflow California linear regression

--- HW0/Q1/q1.py
+++ HW0/Q1/q1.py
@@ -1,19 +1,124 @@
+from __future__ import print_function
+import math
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
 
-with open("data/matrixA.txt", "r") as lines:
-    matrixA = lines.read().split(',')
-    matrixA = list(map(int, matrixA))
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+california_housing_dataframe = pd.read_csv(
+    "https://download.mlcc.google.cn/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe = california_housing_dataframe.reindex(
+    np.random.permutation(california_housing_dataframe.index)
+)
+california_housing_dataframe["median_house_value"] /= 1000
+print(california_housing_dataframe)
 
-with open("data/matrixB.txt", "r") as lines:
-    matrixB = lines.readlines()
-    matrixB = [line.strip() for line in matrixB]
-    matrixB = [list(map(int, line.split(','))) for line in matrixB]
+# define input feature: total_rooms
+my_feature = california_housing_dataframe[["total_rooms"]]
+feature_columns = [tf.feature_column.numeric_column("total_rooms")]
 
-A = np.array(matrixA)
-B = np.array(matrixB)
+# define label
+targets = california_housing_dataframe["median_house_value"]
 
-C = A.dot(B)
-C = np.sort(C)
+my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
 
-np.savetxt("ans_one.txt", C, fmt="%d")
+linear_regressor = tf.estimator.LinearRegressor(
+    feature_columns=feature_columns,
+    optimizer=my_optimizer
+)
 
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    features = {key: np.array(value) for key, value in dict(features).items()}
+    ds = Dataset.from_tensor_slices((features, targets))
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    if shuffle:
+        ds = ds.shuffle(buffer_size=10000)
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+
+_ = linear_regressor.train(
+    input_fn=lambda: my_input_fn(my_feature, targets),
+    steps=100
+)
+
+prediction_input_fn = lambda: my_input_fn(my_feature, targets, num_epochs=1, shuffle=False)
+
+predictions = linear_regressor.predict(input_fn=prediction_input_fn)
+
+predictions = np.array([item["predictions"][0] for item in predictions])
+
+mean_squared_error = metrics.mean_squared_error(predictions, targets)
+root_mean_squared_error = math.sqrt(mean_squared_error)
+print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
+print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
+
+calibration_data = pd.DataFrame()
+calibration_data["predictions"] = pd.Series(predictions)
+calibration_data["targets"] = pd.Series(targets)
+calibration_data.describe()
+
+sample = california_housing_dataframe.sample(n=300)
+
+# Get the min and max total_rooms values.
+x_0 = sample["total_rooms"].min()
+x_1 = sample["total_rooms"].max()
+
+# Retrieve the final weight and bias generated during training.
+weight = linear_regressor.get_variable_value('linear/linear_model/total_rooms/weights')[0]
+bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
+
+# Get the predicted median_house_values for the min and max total_rooms values.
+y_0 = weight * x_0 + bias
+y_1 = weight * x_1 + bias
+
+# Plot our regression line from (x_0, y_0) to (x_1, y_1).
+plt.plot([x_0, x_1], [y_0, y_1], c='r')
+
+# Label the graph axes.
+plt.ylabel("median_house_value")
+plt.xlabel("total_rooms")
+
+# Plot a scatter plot from our data sample.
+plt.scatter(sample["total_rooms"], sample["median_house_value"])
+
+# Display graph.
+plt.show()
+
+# with open("data/matrixA.txt", "r") as lines:
+#     matrixA = lines.readlines()
+#     matrixA = [line.strip() for line in matrixA]
+#     matrixA = [list(map(int, line.split(','))) for line in matrixA]
+#     print(matrixA)
+#
+# with open("data/matrixB.txt", "r") as lines:
+#     matrixB = lines.readlines()
+#     matrixB = [line.strip() for line in matrixB]
+#     matrixB = [list(map(int, line.split(','))) for line in matrixB]
+#     print(matrixB)
+#
+# A = np.array(matrixA)
+# B = np.array(matrixB)
+# C = A.dot(B)
+# C = np.sort(C)
+#
+# print(C)
+#
+# try:
+#     tf.contrib.eager.enable_eager_execution()
+# except ValueError:
+#     pass  # enable_eager_execution errors after its first call
+#
+# tensor = tf.constant('Hello, world!')
+# tensor_value = tensor.numpy()
+# print(tensor_value)
